dfb753b6746de6992a7190fc3fa93fdcd1112e59
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 <e.testagrossa@digitalpa.it>
2021-09-02 17:14:39 +0200

    primo

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'body'];
+    //con Livewire ciò che serve del Model sono solo i campi fillable
+}
